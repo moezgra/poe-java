05191525b75c131ac5af1f841ac9d8b1b8a5d6ef
moezgra <72264171+moezgra@users.noreply.github.com>
2020-10-07 10:10:36 +0200

    Update Main.java

--- projet-eclipse/src/Main.java
+++ projet-eclipse/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.err.println("Hello world from eclipse");
+				System.err.println("Hello world from github");
+
 
 	}
 
